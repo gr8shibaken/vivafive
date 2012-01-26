b61abfdcea19e4ead25950be9f6be9afe16174aa
Shinpei Kawahito <kawahito.shinpei@gmail.com>
2012-01-26 18:02:18 +0900

    create rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vivafive::Application.config.secret_token = '1fba932b09a228db4fe7ec3fe5943d014860ec876216cf5da2b5d0cd1735d3c24f2b06e08cddb9c8c52e893d0d5ef64a0692b6220e02702fab070bfff4f8df52'
